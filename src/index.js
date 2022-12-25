21f8485c2c3e539ca02771ec4988a6149dc5b074
Vitalii89vn <vitalii.kuhta@gmail.com>
2022-12-25 17:16:03 +0200

    first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// let lightbox = new SimpleLightbox('.gallery a');
+
+const refs = {
+    form: document.querySelector('#search-form'),
+    searchBtn: document.querySelector('.search'),
+    loadMoreBtn: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery'),
+}
+
+refs.form.addEventListener('submit', onSearchImages);
+refs.loadMoreBtn.addEventListener('click', onClickLoadMoreBtn);
+
+function onSearchImages(e) {
+    e.preventDefault();
+    console.log('ураа')    
+}
